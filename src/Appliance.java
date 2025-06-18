373679e4c45e5116d3dfcdceb38a2ad1c2dca02c
Ryan <103644734+MrIvory124@users.noreply.github.com>
2025-06-19 11:07:00 +1200

    Fixing broken branch

---
+++ src/Appliance.java
@@ -0,0 +1,54 @@
+/**
+ * @author RyanBulcraig
+ */
+public class Appliance {
+
+    private final String category;
+    private final float price;
+    private final String name;
+
+    /**
+     * The constructor method for this class.
+     * Order is 1. Category, 2. Price, 3. Name
+     */
+    public Appliance(String category, float price, String name) {
+        this.category = category;
+        this.price = price;
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Compares 2 appliance objects. Starts lexicographically, then moves to price (cheap to expensive), finishes with name lexicographically.
+     * @param other The other appliance to be checked against this one
+     * @return negative means less, 0 means same, positive means greater than
+     */
+    public int compareTo(Appliance other) {
+        int categoryCompare = getCategory().compareToIgnoreCase(other.getCategory());
+
+        if (categoryCompare == 0) { // when they are the same, sort by name
+            if (getPrice() == other.getPrice()) {
+                return getName().compareTo(other.getName());
+            } else { // else sort by price
+                return getPrice() < other.getPrice() ? -1 : 1;
+            }
+        }
+        return categoryCompare;
+    }
+
+    public String toString() {
+        return "%-20s | %-40s | %s".formatted(getCategory(), getName(), getPrice());
+    }
+
+}
